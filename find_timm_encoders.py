9c3e1749b44ebd7a2b2d2308fb3c1fda709dbae4
hoa <huynhlequang@gmail.com>
2025-03-02 15:39:26 +0900

    update report format

--- find_timm_encoders.py
+++ find_timm_encoders.py
@@ -9,31 +9,27 @@ if __name__ == '__main__':
     
     parser = argparse.ArgumentParser(description='Encoder check.', fromfile_prefix_chars='@')
     parser.add_argument('-q', '--query',                   type=str,   help='query keyword', default='*clip*')
-    parser.add_argument('-iw', '--input_width', help="Input width", type=int, default = 224)
-    parser.add_argument('-ih', '--input_height', help="Input height", type=int, default = 224)
+    parser.add_argument('-iw', '--width', help="Input width", type=int, default=224)
+    parser.add_argument('-ih', '--height', help="Input height", type=int, default=224)
     parser.add_argument('-r', '--report', help="Export report", action='store_true')
     args = parser.parse_args()
-
-    query = args.query
-    width = args.input_width
-    height = args.input_height
         
     report = None
     if args.report:
-        report = open("report.txt", "w+")
+        report = open(f"{args.query}_{args.width}_{args.height}.txt", "w+")
 
-    model_names = timm.list_models(query)
+    model_names = timm.list_models(args.query)
     if len(model_names) == 0:
-        text = f"No model found with query {query}"
+        text = f"No model found with query {args.query}"
         logger.error(text)
         if args.report:
             report.write(f"{text}\n")
     for model_name in model_names:
         try:
             model = timm.create_model(model_name, features_only=True, pretrained=False)
-            image = torch.randn(1, 3, height, width)
+            image = torch.randn(1, 3, args.height, args.width)
             features = model(image)
-            text = f'{model_name} supports up to {len(features)}-level features with input shape (1, 3, {height}, {width}):'
+            text = f'{model_name} supports up to {len(features)}-level features with input shape (1, 3, {args.height}, {args.width}):'
             logger.info(text)
             if args.report:
                 report.write(f"{text}\n")
@@ -43,7 +39,7 @@ if __name__ == '__main__':
                 if args.report:
                     report.write(f"{text}\n")
         except:
-            text = f'{model_name} does not support feature extractor with input shape (1, 3, {height}, {width})'
+            text = f'{model_name} does not support feature extractor with input shape (1, 3, {args.height}, {args.width})'
             logger.warning(text)
             if args.report:
                 report.write(f"{text}\n")
